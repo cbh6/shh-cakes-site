c759923229b3e6e8de2e4417e062c7b3096f5d41
Cristian <cristian@digio.es>
2019-01-04 21:04:51 +0100

    Layout design touches

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@ import pic01 from '../assets/images/pic01.jpg'
 import pic02 from '../assets/images/pic02.jpg'
 import pic03 from '../assets/images/pic03.jpg'
 import pic04 from '../assets/images/pic04.jpg'
+import baker from '../assets/images/baker.png'
+import cake from '../assets/images/cake.png'
 
 class Homepage extends React.Component {
   render() {
@@ -17,24 +19,34 @@ class Homepage extends React.Component {
         <Helmet title={siteTitle} />
 
         <section className="hero-header">
-          <h2>Tartas. Eslogan para el site</h2>
-          <h3>Nombre repostería</h3>
+          <img src={cake} alt="Piece of cake" className="hero-header__img" />
+          <div>
+            <h1>La tarta de Susi</h1>
+            <h2>Especialidad en elaboración de tartas tradicionales</h2>
+            <p>En esta web encontrarás algunas de las tartas que realizo</p>
+          </div>
         </section>
 
+        {/* ---- Section: About me ---- */}
         <section className="about">
           <h2>Sobre mí</h2>
-          <img className="about__avatar" />
+          <img className="about__avatar" src={baker} alt="baker avatar" />
           <p className="about__description">
             Me llamo María Asunción y me dedico a hacer tartas. Soy autodidacta
             y en mis ratos libres me gusta realizar todo tipo de repostería.
           </p>
+          <a href="https://www.freepik.com/free-vector/set-of-bakery-banners_1970336.htm">
+            Designed by Freepik
+          </a>
+          <a href="https://www.freepik.com/free-vector/minimal-triangle-pattern-design-in-pastel-colors_2841787.htm">
+            Designed by Starline
+          </a>
           {/* <p>Si deseas saber más o realizar algún pedido no dudes en ponerte en contacto conmigo</p> */}
         </section>
 
+        {/* ---- Section: cakes ---- */}
         <section className="cakes">
           <h2>Tartas y dulces que realizo</h2>
-          <br />
-
           <div className="cake__item">
             <img className="cake__photo" />
             <div className="cake__info">
@@ -120,13 +132,13 @@ class Homepage extends React.Component {
                 rows="6"
               />
             </div>
-            <button
-              type="submit"
-              className="contact__button"
-            >
+            <button type="submit" className="contact__button">
               Enviar
             </button>
           </form>
+          <a href="https://www.freepik.com/free-vector/hand-drawn-bakery-products-pattern_836138.htm">
+            Designed by Freepik
+          </a>
         </section>
       </Layout>
     )
